899cc9ac525c560557afba4e450a00ec5809aa78
Maxime Rauer <maxime.rauer@gmail.com>
2020-10-15 20:50:28 -0500

    processed ok

--- run.py
+++ run.py
@@ -3,7 +3,10 @@ import os
 import operator
 from collections import OrderedDict
 import re
-#import statistics 
+import statistics 
+
+SCORE_BIAS = -0.35
+FRAMES_GAP = 17
 
 # Load from JSON file
 f = open('input.json', 'r')
@@ -15,23 +18,21 @@ for item in items:
     _id = int(re.findall(r'\d+', item['image_id'])[0])
     sorted_items[_id] = float(item['mean_score_prediction'])
 
-#avg_value = statistics.mean(sorted_items.values())
-#print(avg_value)
+avg_value = statistics.mean(sorted_items.values())
+print(avg_value)
 
 good_idx = set()
 last_good_key = 0
 temp_idx = dict()
 for key, value in sorted_items.items():
-    #print key
-    if value > 5.3:
+    if value > avg_value - SCORE_BIAS:
         temp_idx[key] = value
-        if key > last_good_key + 30:
-             #good_idx.add(max(temp_idx.items(), key=operator.itemgetter(1))[0])
+        if key > last_good_key + FRAMES_GAP:
              good_idx.add(max(temp_idx.items(), key=lambda k: k[1])[0])
              temp_idx = dict()
         last_good_key = key
 print(good_idx)
 
-#for key, value in sorted_items.items():
-#    if key not in good_idx:
-#        os.remove('frames/out-'+str(key)+'.jpg')
+for key, value in sorted_items.items():
+    if key not in good_idx:
+        os.remove('frames/out-'+str(key)+'.jpg')
