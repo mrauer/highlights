bb6bbe3bef3046225d88dd589d392d75a85e20e0
Maxime Rauer <maxime.rauer@gmail.com>
2020-10-28 21:00:46 -0500

    [MID] ffmpeg in container (#7)
    * [MID] ffmpeg in container
    
    * with comments

--- lib/entrypoints/entrypoint.predict.cpu.sh
+++ lib/entrypoints/entrypoint.predict.cpu.sh
@@ -4,6 +4,15 @@ set -e
 BASE_MODEL_NAME=$1
 WEIGHTS_FILE=$2
 IMAGE_SOURCE=$3
+VIDEO_SOURCE=$4
+
+ACTION=$(echo $PREDICT_MODEL| cut -d'_' -f 3)
+VIDEO_FILE=$(ls -1t /src/source|head -n 1)
+
+if [ $ACTION == "technical" ]
+then
+  ffmpeg -i /src/source/$VIDEO_FILE -qscale:v 2 "/src/frames/out-%01d.jpg"
+fi
 
 # predict
 python -m evaluater.predict \
