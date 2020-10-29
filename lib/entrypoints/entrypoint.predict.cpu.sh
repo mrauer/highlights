d8d1c4b47118b850d56cef6de7c9d2d7fbf1ea83
Maxime Rauer <maxime.rauer@gmail.com>
2020-10-28 21:00:00 -0500

    with comments

--- lib/entrypoints/entrypoint.predict.cpu.sh
+++ lib/entrypoints/entrypoint.predict.cpu.sh
@@ -9,13 +9,8 @@ VIDEO_SOURCE=$4
 ACTION=$(echo $PREDICT_MODEL| cut -d'_' -f 3)
 VIDEO_FILE=$(ls -1t /src/source|head -n 1)
 
-echo $PREDICT_MODEL
-echo $ACTION
-
-#if [[ $ACTION -eq "technical" ]]
 if [ $ACTION == "technical" ]
 then
-  echo "HERE WE START FFMEG"
   ffmpeg -i /src/source/$VIDEO_FILE -qscale:v 2 "/src/frames/out-%01d.jpg"
 fi
 
